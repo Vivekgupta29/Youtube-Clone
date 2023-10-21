6bbe9ebc296cbc4272c7912ca048b97a1871c95b
Vivek Gupta <40853271+Vivekgupta29@users.noreply.github.com>
2023-10-21 12:02:27 +0000

    rr

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+function Main(){
+    return (
+        <>
+            <H1 className="bg">Hello World</H1>
+        </>
+    );
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "../src/index.css";
+
+function Navbar(){
+    return (
+        <>
+            <H1>Hello World</H1>
+        </>
+    );
+}
+
+export default Navbar
